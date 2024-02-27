509211c6befcb66343ecf263008561afc4418e52
sabo110 <samirphanuel@gmail.com>
2024-02-27 16:22:01 +0100

    bd creer

--- portfolio/projects/models.py
+++ portfolio/projects/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Tech(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Project(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=False)
+    thumbnail = models.ImageField(blank=False, null=False, upload_to="thumbnail")
+    description = models.TextField(blank=False, null=False)
+    demo_link = models.CharField(max_length=500, null=False, blank=False)
+    github_link = models.CharField(max_length=500, null=True, blank=True)
+    tech = models.CharField(max_length=200, null=False, blank=False)
+
+
+
+
