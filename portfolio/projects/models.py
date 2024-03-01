96c6d1ae0def424e449eb63c4aa552464cad924a
sabo110 <samirphanuel@gmail.com>
2024-03-01 19:07:13 +0100

    ajout d'un signale pour supprimer le fichier physqiue image apres la suppresion d'un projet

--- portfolio/projects/models.py
+++ portfolio/projects/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 # Create your models here.
+import os
 
 class Tech(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
@@ -15,6 +17,20 @@ class Project(models.Model):
     demo_link = models.CharField(max_length=500, null=False, blank=False)
     github_link = models.CharField(max_length=500, null=True, blank=True)
     techs = models.ManyToManyField(Tech)
+
+@receiver(post_delete, sender=Project)
+def delete_thumbnail(sender, instance, **kwargs):
+    """
+    Supprime le fichier thumbnail du système de fichiers
+    quand l'instance de Project correspondante est supprimée.
+    """
+    # Vérifiez si un thumbnail existe
+    if instance.thumbnail:
+        # Construisez le chemin complet du fichier
+        file_path = instance.thumbnail.path
+        # Supprimez le fichier
+        if os.path.isfile(file_path):
+            os.remove(file_path)
 
     
 
